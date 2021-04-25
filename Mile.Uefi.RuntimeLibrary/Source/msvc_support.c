3ec96076d67efdadb528e53f4bf4a36c46464729
MouriNaruto <Mouri_Naruto@Outlook.com>
2021-04-25 11:40:24 +0800

    Add float.h and limits.h.

---
+++ Mile.Uefi.RuntimeLibrary/Source/msvc_support.c
@@ -0,0 +1,13 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      msvc_support.cpp
+ * PURPOSE:   Implementation for MSVC support.
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+// This is required to keep VC++ happy if you use floating-point
+int _fltused = 1;
+int __sse2_available = 0;   ///< Used by ftol2_sse
